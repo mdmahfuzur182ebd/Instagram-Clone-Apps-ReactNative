2b04d90206916751c987dc79bec59ee9cc18176d
Mahfuzur <mdmahfuzur7788@gmail.com>
2022-06-27 02:35:49 +0600

    Something added About.js file and also create a MenuItem.js file inital function added

--- components/restaurantDetail/About.js
+++ components/restaurantDetail/About.js
@@ -1,11 +1,53 @@
 import React from 'react';
+import { Image } from 'react-native';
 import { View, Text } from 'react-native';
 
+const image ="https://static.onecms.io/wp-content/uploads/sites/9/2020/04/24/ppp-why-wont-anyone-rescue-restaurants-FT-BLOG0420.jpg";
+
+ const title = 'Farmhouse Kitchen Thai Cuisine';
+ const description = 'Thai . Comfort Food . $$ • 🎫 • 4 ⭐ (2913+)'; 
 
 export default function About() {
   return (
     <View>
-      <Text>About</Text>
+        <RestaurantImage image={image}/>
+        <RestaurantTitle title={title}/>
+        <RestaurantDescription description={description} />
     </View>
-  )
+  );
 }
+
+const RestaurantImage = (props) => (
+
+    <Image source={{uri:props.image}} style={{width:'100%', height:180 }} />
+);
+
+
+const RestaurantTitle = (props) => (
+    <Text style={{
+         fontSize:29,
+         fontWeight:"600",
+         marginTop:10,
+         marginHorizontal:15,    
+    }}
+   >
+    {props.title}
+  </Text>
+
+);
+
+
+const RestaurantDescription =(props) => (
+    <Text
+      style={{
+        marginTop:10,
+        marginHorizontal:15,
+        fontWeight: '400',
+        fontSize: 15.5,
+      }}
+    >
+     {props.description}
+    
+  </Text>
+
+);
