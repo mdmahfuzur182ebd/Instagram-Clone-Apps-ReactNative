7b81b6db889cef8a1b957aafc37c92f2a9c60731
Mahfuzur <mdmahfuzur7788@gmail.com>
2022-06-26 17:59:21 +0600

    yelp api used 80% work and work in search bar

--- screens/Home.js
+++ screens/Home.js
@@ -1,4 +1,5 @@
-import React from 'react';
+// import React from 'react';
+import React, { useState, useEffect } from "react";
 import { View, Text , SafeAreaView, Platform,ScrollView} from 'react-native';
 import Constants from 'expo-constants';
 import HeaderTabs from '../components/HeaderTabs';
@@ -6,15 +7,15 @@ import SearchBar from '../components/SearchBar';
 import Categories from '../components/Categories';
 import RestaurantItems, { localRestaurants } from '../components/RestaurantItems';
 
-const YELP_API_KEY = 
-"KgxlkrTw2rlbtBUFEX9Z7lvBZF1GH4vHnbuH0fC7TzBHANWTtWkk4uSoR5R93nIsf1IkekyLTCISg3VaY-X-ZTuneLQ-_jzvJ24hWib265Dc7FH--fAnPi64az64YnYx "
+const YELP_API_KEY = "KgxlkrTw2rlbtBUFEX9Z7lvBZF1GH4vHnbuH0fC7TzBHANWTtWkk4uSoR5R93nIsf1IkekyLTCISg3VaY-X-ZTuneLQ-_jzvJ24hWib265Dc7FH--fAnPi64az64YnYx ";
 
 export default function Home() {
+
 const [restaurantData, setRestaurantData] = React.useState(localRestaurants)
 
 const getRestaurantFromYelp = () => {
   
-    const yelpUrl = `https://api.yelp.com/v3/businesses/search?term=restaurants&location=${city}`;
+    const yelpUrl = `https://api.yelp.com/v3/businesses/search?term=restaurants&location=LosAngeles}`;
 
  
 
@@ -26,10 +27,14 @@ const getRestaurantFromYelp = () => {
     
   return fetch(yelpUrl,apiOptions)
   .then( (res) => res.json()).
-  then(json => 
+  then((json) => 
     setRestaurantData(json.businesses));
 
 };
+
+  useEffect(() => {
+     getRestaurantFromYelp();
+  }, []);
 
 
 
