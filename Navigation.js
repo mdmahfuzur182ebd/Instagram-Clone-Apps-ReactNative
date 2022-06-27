4a97fedc3ebfe99836cbed028ee77a2ed93cb435
Mahfuzur <mdmahfuzur7788@gmail.com>
2022-06-27 11:54:49 +0600

    Navstion 10% work

--- Navigation.js
+++ Navigation.js
@@ -2,16 +2,26 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
+import Home from './screens/Home';
+import RestaurantDetail from "./screens/RestaurantDetail";
 
 
 export default function Navigation() {
-     const stack = createStackNavigator();
+     const Stack = createStackNavigator();
 
      const screenOption ={
         headerShown: false,
-     }
+     };
 
   return (
-     <NavigationContainer/>
-  )
+     <NavigationContainer>
+
+        <Stack.Navigator screenOption={screenOption}>
+             <Stack.Screen name="Home" component={Home} />
+             <Stack.Screen name="RestaurantDetail" component={RestaurantDetail} />
+
+        </Stack.Navigator>
+
+     </NavigationContainer>
+  );
 }
