63e6dbfdd6ccc94dd2451b6fb7f1476aaac61708
Mahfuzur <mdmahfuzur7788@gmail.com>
2022-06-27 16:32:38 +0600

    ViewCart not show

--- screens/RestaurantDetail.js
+++ screens/RestaurantDetail.js
@@ -3,15 +3,17 @@ import { View, Text } from 'react-native';
 import { Divider } from 'react-native-elements';
 import About from '../components/restaurantDetail/About';
 import MenuTtems from '../components/restaurantDetail/MenuTtems';
+import ViewCart from '../components/restaurantDetail/ViewCart';
 
 
 
-export default function RestaurantDetail({ route }) {
+export default function RestaurantDetail({ route, navigation }) {
   return (
     <View>
       <About route={route} />
       <Divider width={1.8} style={{ marginVertical:20 }} />
       <MenuTtems />
+      <ViewCart navigation={navigation} restaurantName={route.params.name} />
     </View>
   )
 }
