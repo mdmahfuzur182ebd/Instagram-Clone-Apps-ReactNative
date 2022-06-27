366383e37bb7e48fc515aa2754a134ba248bb515
Mahfuzur <mdmahfuzur7788@gmail.com>
2022-06-27 16:08:28 +0600

    we are create ViewCart.js file

---
+++ components/restaurantDetail/ViewCart.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+
+export default function ViewCart() {
+  return (
+    <View>
+      <Text>ViewCart</Text>
+    </View>
+  )
+}
