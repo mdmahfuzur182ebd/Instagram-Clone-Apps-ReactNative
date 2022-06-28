c5f312f14110bfd9701992efb50a08cfca9e3c98
Mahfuzur Rahman <mdmahfuzur980@gmail.com>
2022-06-28 21:04:06 +0600

    We are create redux file and alw create reducerfile and created sotre.js and index.js & cartReducer.js

--- components/restaurantDetail/About.js
+++ components/restaurantDetail/About.js
@@ -1,20 +1,8 @@
 import React from 'react';
 import { View, Text, Image  } from 'react-native';
 
-const yelpRestaurantInfo = {
-        name: "Farmhouse Kitchen Thai Cuisine",
-        image: "https://static.onecms.io/wp-content/uploads/sites/9/2020/04/24/ppp-why-wont-anyone-rescue-restaurants-FT-BLOG0420.jpg",
-        price:'$$',
-        reviews : "1300",
-        rating: 4.5,
-        categories: [
-          {title: "Bangladish"},
-          {title:"Comfort Food"},
-          {title:"Coffee"},
-          {title:"Ice Cream"},
-          {title:"Snacks"},
-      ],
-};
+
+
 
 export default function About(props) {
    const { name, image, price, reviews, rating, categories } = props.route.params ;
