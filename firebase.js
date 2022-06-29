bbd5bfee21726135358abcbff5ebf44f644961f7
Mahfuzur Rahman <mdmahfuzur980@gmail.com>
2022-06-29 23:35:21 +0600

    install firebase and create project and conn to firebase.js file

---
+++ firebase.js
@@ -0,0 +1,30 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBZzoDEv430uoxGWvC6BFw1UmCq82wXUEQ",
+  authDomain: "reactnative-uber-eats-clone.firebaseapp.com",
+  projectId: "reactnative-uber-eats-clone",
+  storageBucket: "reactnative-uber-eats-clone.appspot.com",
+  messagingSenderId: "5233605810",
+  appId: "1:5233605810:web:a7f2bca8318131bd069dab",
+};
+
+
+!firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
+
+export default firebase;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
