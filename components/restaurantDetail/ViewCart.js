d9d7033711d9e3f70afb207a78c70484af98dcab
Mahfuzur Rahman <mdmahfuzur980@gmail.com>
2022-06-29 21:06:19 +0600

    install npm reanimated and also creat OrderItem.js

--- components/restaurantDetail/ViewCart.js
+++ components/restaurantDetail/ViewCart.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity , Modal} from 'react-native';
 import { useSelector } from 'react-redux';
+import { useState } from 'react';
 
 
 export default function ViewCart() {
+  
+  const [ modalVisible, setModalVisible ] = useState(false);
 
-  const items = useSelector((state) => state.cartReducer.selectedItems.items);
+  const[items, restaurantName ] = useSelector((state) => state.cartReducer.selectedItems.selectedItems);
 
   const total = items
     .map((item) => Number(item.price.replace("$", "")))
@@ -18,10 +21,80 @@ export default function ViewCart() {
 
   }); 
 
-  console.log(totalUSD);
+   const styles = StyleSheet.create({
+       modalContainer: {
+            flex: 1,
+            justifyContent: 'flex-end',
+            backgroundColor: "rgba(0,0,0,0.7)",
+       },
+       modalCheckoutContainer :{
+             backgroundColor: 'white',
+             padding: 16,
+             height: 500,
+             borderWidth:1,
+       },
 
-return (
+      modalCheckoutButton:{
+         textAlign : "center",
+         fontWeight: "600",
+         fontSize:18,
+         marginBottom: 10,
+      },
+      subtotalContainer :{
+        flexDirection :"row",
+        justifyContent: "space-between",
+        marginTop: 15,
+
+      },
+      subtotalText:{
+        textAlign:"left",
+        fontWeight: "600",
+        fontSize: 15,
+        marginBottom: 10,
+       },
+   });
+
+
+   const checkoutModalContent = () => {
+      return(
+       <View 
+       style={{
+           flex:1,
+           justifyContent:"center",
+           alignItems: 'center',
+           marginTop: 30,
+
+       }}>
+        <View 
+         style={{
+            backgroundColor:'black',
+            padding: 10,
+            borderRadius:30,
+            width: 150,
+            alignItems:"center",
+
+        }}
+        >
+        <TouchableOpacity onPress={() => setModalVisible(false) }>
+           <Text style={{color:'white'}}>Checkout</Text>
+        </TouchableOpacity>
+         </View>
+       </View>
+      );
+   };
+
+  // console.log(totalUSD);
+
+ return (
    <>
+      <Modal 
+      animationType='slide' visible={modalVisible}
+      transparent ={true}
+      onRequestClose={() => setModalVisible(false)}
+      >
+       {checkoutModalContent()}
+      </Modal>
+
     {total ? (
     <View 
     style={{
@@ -54,6 +127,7 @@ return (
              position:'relative',
 
           }}
+          onPress={() => setModalVisible(true)}
          >
          <Text style={{ color: "white", fontSize: 20, marginRight: 30 }}>
               View Cart
